edfd38d63db5ff9f6d4954fbc82e50a93cd44584
sp <spboke.xyz@gmail.com>
2018-04-15 05:31:49 +0000

    Users can edit their own topic

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -32,24 +32,29 @@
                                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                                     <div class="form-group">
-                                        <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
+                                        <input class="form-control" type="text" name="title"
+                                               value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
                                     </div>
 
                                     <div class="form-group">
                                         <select class="form-control" name="category_id" required>
                                             <option value="" hidden disabled selected>请选择分类</option>
                                             @foreach ($categories as $value)
-                                                <option value="{{ $value->id }}">{{ $value->name }}</option>
+                                                <option {{ $topic->category_id==$value->id?'selected':'' }} value="{{ $value->id }}">{{ $value->name }}</option>
                                             @endforeach
                                         </select>
                                     </div>
 
                                     <div class="form-group">
-                                        <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+                                        <textarea name="body" class="form-control" id="editor" rows="3"
+                                                  placeholder="请填入至少三个字符的内容。"
+                                                  required>{{ old('body', $topic->body ) }}</textarea>
                                     </div>
 
                                     <div class="well well-sm">
-                                        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
+                                        <button type="submit" class="btn btn-primary"><span
+                                                    class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存
+                                        </button>
                                     </div>
                                 </form>
                 </div>
@@ -57,25 +62,25 @@
         </div>
     </div>
 @section('scripts')
-    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
 
     <script>
-      $(document).ready(function(){
+      $(document).ready(function () {
         var editor = new Simditor({
           textarea: $('#editor'),
-          upload:{
+          upload: {
             url: '{{ route('topics.upload_image') }}',
-            params: { _token: '{{ csrf_token() }}' },
+            params: {_token: '{{ csrf_token() }}'},
             fileKey: 'upload_file',
             connectionCount: 3,
             leaveConfirm: '文件上传中，关闭此页面将取消上传。'
           },
           pasteImage: true,
-        });
-      });
+        })
+      })
     </script>
 
 @stop
